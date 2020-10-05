1d488fa060cce1d6ed9b8e6ccd9895e84220c294
igavelyuk <gavelyukworking@gmail.com>
2020-10-06 00:28:28 +0300

    megaupdate

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
+import { render } from 'react-dom';
+import JobsPage from './pages/jobs.page';
+import LoginPage from './pages/login.page';
+import RegisterPage from './pages/register.page';
+import {
+  HashRouter as Router,
+  Route,
+  Link,
+  Switch,
+} from "react-router-dom";
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{color: "red"}}>
+      <h1>Please Scroll</h1>
+      </div>
+      <Router>
+       <Link to="/" activeClassName="active">LoginPage</Link>
+       <Link to="/register" activeClassName="active">RegisterPage</Link>
+       <Link to="/jobs" activeClassName="active">JobsPage</Link>
+
+       <Switch>
+         <Route path="/" component={LoginPage} exactt></Route>
+         <Route path="/register" component={RegisterPage} exact></Route>
+         <Route path="/jobs" component={JobsPage} exact ></Route>
+       </Switch>
+     </Router>
+      // <Router>
+      //   <Route path="/" component={LoginPage} exact/>
+      //   <Route path="/register" component={RegisterPage} exact/>
+      //   <Route path="/jobs" component={JobsPage} exact />
+      // </Router>
     </div>
   );
 }
